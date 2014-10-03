4b59aa7cb9e02e8d0e1dae6ffb0670694773db5a
Cristina Sturm <cristurm@hotmail.com>
2014-10-02 23:51:55 -0300

    Apply changes to index and cleanup code

--- js/nyan.js
+++ js/nyan.js
@@ -1,23 +1,17 @@
 console.log('Nyan!');
 
 $(function() {
-	var flag = false,
-		nyancat = new NyanCat(),
-		sparks = new Sparks(),
-		rainbow = new Rainbow();
+	var nyancat = new NyanCat(),
+		sparks = new Sparks();
 
 	nyancat.init();
 	sparks.init();
-	rainbow.init();
 
 	var timer = setInterval(function () {
 		nyancat.cycleFrames();
-		rainbow.cycleFrames(flag);
 
 		sparks.allCombos.each(function () {
 			sparks.moveSparksCombo($(this));
 		});
-
-		flag = !flag;
 	}, 70);
 });
