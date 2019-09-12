2188c95ea7db7dd9d319ec0568a13f45d1df1d03
Cristina Sturm <cristurm90@gmail.com>
2019-09-12 16:42:53 -0300

    Anihilates jQuery.

--- js/nyan.js
+++ js/nyan.js
@@ -3,14 +3,15 @@ console.log('Nyan!');
 var NyanCat = function () {
 	return {
 		init: function () {
-			this.cat = $('#nyan-cat');
+			this.cat = document.getElementById('nyan-cat');
 			this.framesAmount = 6;
 			this.currentFrame = 1;
 		},
 
 		cycleFrames: function () {
 			var myself = this;
-			this.cat.removeClass('frame' + myself.currentFrame).addClass('frame' + myself.cycleIds(myself.currentFrame));
+			this.cat.classList.remove('frame' + myself.currentFrame);
+			this.cat.classList.add('frame' + myself.cycleIds(myself.currentFrame));
 			this.currentFrame = this.cycleIds(this.currentFrame);
 		},
 
@@ -29,28 +30,28 @@ var NyanCat = function () {
 var Sparks = function () {
 	return {
 		init: function (_combo) {
-			var yCombosAmount = Math.ceil($(document).height() / _combo.height()),
-					comboTags = $(document.createElement('div')),
+			var yCombosAmount = Math.ceil(document.body.offsetHeight / _combo.offsetHeight),
+					comboTags = document.createElement('div'),
 					newCombo = null;
 
 			for (var a = 0; a < yCombosAmount-1; a += 1) {
-				newCombo = _combo.clone();
+				newCombo = _combo.cloneNode(true);
 				comboTags.append(newCombo); // <- still have to improve this crap
 			}
 
-			$('body').prepend(comboTags.html());
+			document.body.prepend(comboTags);
 		}
 	}
 };
 
-$(function() {
+(function () {
 	var nyancat = new NyanCat(),
 			sparks = new Sparks();
 
 	nyancat.init();
-	sparks.init($('.sparks-combo'));
+	sparks.init(document.querySelector('.sparks-combo'));
 
 	var timer = setInterval(function () {
 		nyancat.cycleFrames();
 	}, 70);
-});
+})();
